cd98c934b41d96ea8323f88a69759587569a1530
Omkar Dhananjay Satav <69974754+OmkarSatav@users.noreply.github.com>
2022-11-30 08:03:41 +0530

    Update Stock_Purchase_Sell_Val.java

--- 1-Arrays/Stock_Purchase_Sell_Val.java
+++ 1-Arrays/Stock_Purchase_Sell_Val.java
@@ -43,6 +43,19 @@ public class Stock_Purchase_Sell_Val {
         return profit;
     }
     
+    // Another way
+     public int maxProfit1(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        
+        for(int i=0; i<prices.length; i++){
+            minPrice= Math.min(minPrice,prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        
+        return maxProfit;
+    }
+    
     
     public static void main(String[] args) {
         int stockPriceOnDay[] = { 7, 1, 5, 3, 6, 4 };

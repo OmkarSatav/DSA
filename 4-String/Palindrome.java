b4b9b8c1a6a387b671ae0e22dd5649996f1261bb
Omkar Dhananjay Satav <69974754+OmkarSatav@users.noreply.github.com>
2022-12-01 17:19:47 +0530

    Checks all types of Strings

--- 4-String/Palindrome.java
+++ 4-String/Palindrome.java
@@ -10,8 +10,35 @@ public class Palindrome {
         return true;
     }
 
+
+    // Accepts all strings
+    // "A man, a plan, a canal: Panama" - Example
+    public static boolean isPalin(String s) {
+        String str = s.toLowerCase();
+        StringBuilder strNew = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (Character.isDigit(c) || Character.isAlphabetic(c)) {
+                strNew.append(c);
+            }
+        }
+
+        int n = strNew.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (strNew.charAt(i) != strNew.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        String str = "mom";
+        String str = "raccar";
         System.out.println("Is the String Palindrome : " + isPalindrome(str));
+
+        str = "A man, a plan, a canal: Panama";
+
+        System.out.println("Is the String '" + str + "' Palindrome : " + isPalin(str));
     }
 }

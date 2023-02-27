9e747c0135e82d8502af2556d12948c1764592f4
Omkar Dhananjay Satav <69974754+OmkarSatav@users.noreply.github.com>
2023-02-27 07:22:10 +0530

    GeeksForGeeks question - Given a Linked list, rearrange it such that converted list should be of the form a < b > c < d > e < f .. where a, b, c are consecutive data node of linked list

---
+++ 7-LinkedList/ZigZag.java
@@ -0,0 +1,37 @@
+class ZigZag{
+
+  
+	public static Node zigZag(Node head){
+        //add code here.
+        
+         if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node temp = head;
+        boolean flag = true;
+        
+        while (temp.next != null) {
+           if(flag){
+               if(temp.data > temp.next.data){
+                   int t = temp.data;
+                   temp.data = temp.next.data;
+                   temp.next.data = t;
+               }
+               flag = !flag;
+           }
+           else{
+               if(temp.data < temp.next.data){
+                   int t = temp.data;
+                   temp.data = temp.next.data;
+                   temp.next.data = t;
+               }
+               flag = !flag;
+           }
+           
+           temp = temp.next;
+        }
+        return head;
+    }
+
+}
